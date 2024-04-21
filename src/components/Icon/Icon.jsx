69586263c225e4aeed775b600592d3e36d22d0c3
rishabh <rishabhsingh2305@gmail.com>
2024-04-21 13:01:35 +0530

    added github redirect

--- src/components/Icon/Icon.jsx
+++ src/components/Icon/Icon.jsx
@@ -1,9 +1,15 @@
 import React from "react";
 
 import loadingGif from "../../assets/loading.gif";
+import github from "../../assets/github.png";
 const Icon = (props) => {
 	const { type = "LOADING", enabled = true } = props;
-	return <>{type === "LOADING" && enabled && <img src={loadingGif} style={{ width: "100%", height: "100%", objectFit: "cover" }} />}</>;
+	return (
+		<>
+			{type === "LOADING" && enabled && <img src={loadingGif} style={{ width: "100%", height: "100%", objectFit: "cover" }} />}
+			{type === "GITHUB" && enabled && <img src={github} style={{ width: "100%", height: "100%", objectFit: "cover" }} />}
+		</>
+	);
 };
 
 export default Icon;
--- src/components/NavBar/Navbar.jsx
+++ src/components/NavBar/Navbar.jsx
@@ -1,9 +1,18 @@
+import Icon from "../Icon/Icon";
 import style from "./Navbar.module.css";
 export default function Navbar(props) {
 	const { title } = props;
+
+	function goToPage(link) {
+		window.open(link, "_blank");
+	}
+
 	return (
 		<div className={style.container}>
 			<div className={style.titleContainer}>{title}</div>
+			<div onClick={() => goToPage("https://github.com/R-I-S-H-A-B-H-S-I-N-G-H/js-serve")} className={style.icon}>
+				<Icon type={"GITHUB"} />
+			</div>
 		</div>
 	);
 }
