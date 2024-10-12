f3ad9cab43cea06293a45dd991e0c0a26e91fec6
R-I-S-H-A-B-H-S-I-N-G-H <rishabhsingh2305@gmail.com>
2024-10-12 18:30:25 +0530

    added support for go backend

--- src/Utils/JsServeApi.js
+++ src/Utils/JsServeApi.js
@@ -2,14 +2,15 @@ import axios from "axios";
 
 const BASE_CDN_URL = "https://s3.us-east-005.backblazeb2.com/testb23";
 // const BASE_URL = "https://js-serve.cyclic.cloud";
-const BASE_URL = "https://jsserve.onrender.com";
+const BASE_URL = "https://go-microservice-k2dn.onrender.com/s3";
+// const BASE_URL = "http://localhost:3000/s3";
 
 export async function upload(props) {
 	const { fileBody, fileName } = props;
 	try {
-		const resp = await axios.post(BASE_URL + "/upload", {
-			fileName: fileName,
-			fileBody: fileBody,
+		const resp = await axios.post(BASE_URL + "/uptos3", {
+			filePath: fileName,
+			fileData: fileBody,
 		});
 		const jsonResp = await resp.data;
 		return jsonResp;
