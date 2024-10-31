c126cc53d0fa8f48333a6f491a40da5c81fe25a2
R-I-S-H-A-B-H-S-I-N-G-H <rishabhsingh2305@gmail.com>
2024-10-31 17:09:05 +0530

    added default theme to light

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
 	return (
 		<html lang="en">
 			<body className={`${geistSans.variable} ${geistMono.variable} antialiased `}>
-				<ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+				<ThemeProvider attribute="class" defaultTheme="light" enableSystem disableTransitionOnChange>
 					<SidebarProvider>{children}</SidebarProvider>
 				</ThemeProvider>
 			</body>
